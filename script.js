933ca76129c188e0b87323981ea97171387703db
Marta <martasagun95@gmail.com>
2024-03-26 21:31:48 +0100

    feat: update plus and minus functions according to technical requirements

--- script.js
+++ script.js
@@ -1,26 +1,74 @@
-const num1 = '20';
-const num2 = '100';
+// const num1 = '90099999999999999999999999099999999999999999999999';
+// const num2 = '99999999999999999999999999999999999999999999999999';
+// num1 = '1239';
+// num2 = '9231';
+// num1 = '92';
+// num2 = '38';
 
 // "plus" function implementation
-String.prototype.plus = function plus(stringValue) {
-	return +this + +stringValue;
-};
-console.log(num1.plus(num2));
+function plus(str1, str2) {
+	let maxLength = Math.max(str1.length, str2.length);
+	let overageValue = 0;
+
+	let result = Array.from({ length: maxLength }, (_, i) => {
+		const digit1 = parseInt(str1[str1.length - i - 1] || '0');
+		const digit2 = parseInt(str2[str2.length - i - 1] || '0');
+		const sum = digit1 + digit2 + overageValue;
+		overageValue = Math.floor(sum / 10);
+		return (sum % 10).toString();
+	})
+		.reverse()
+		.join('');
+
+	if (overageValue > 0) {
+		result = overageValue.toString() + result;
+	}
+
+	return result;
+}
+
+const sumOfNumbers = plus(num1, num2);
+console.log(sumOfNumbers);
 
 // "minus" function implementation
-String.prototype.minus = function minus(stringValue) {
-	return +this > +stringValue ? +this - +stringValue : +stringValue - +this;
-};
-console.log(num1.minus(num2));
+function minus(str1, str2) {
+	const maxLength = Math.max(str1.length, str2.length);
+	let overageValue = 0;
 
-// "divide" function implementation
-String.prototype.divide = function divide(stringValue) {
-	return Math.floor(+this / +stringValue);
-};
-console.log(num1.divide(num2));
+	if (
+		str1.length < str2.length ||
+		(str1.length === str2.length && str1 < str2)
+	) {
+		[str1, str2] = [str2, str1];
+	}
+
+	const result = Array.from({ length: maxLength }, (_, i) => {
+		const digit1 = parseInt(str1[str1.length - 1 - i]);
+		const digit2 = parseInt(str2[str2.length - 1 - i] || '0');
+		let subtr = digit1 - digit2 - overageValue;
+
+		subtr = subtr < 0 ? subtr + 10 : subtr;
+		overageValue = subtr < 0 ? 1 : 0;
+
+		return subtr.toString();
+	})
+		.reverse()
+		.join('');
+
+	return result.replace(/^0+/, '');
+}
+
+const subtractionOfNumbers = minus(num1, num2);
+console.log(subtractionOfNumbers);
 
 // "multiply" function implementation
-String.prototype.multiply = function multiply(stringValue) {
-	return +this * +stringValue;
-};
-console.log(num1.multiply(num2));
+function multiply(str1, str2) {}
+
+const productOfNumbers = multiply(num1, num2);
+console.log(productOfNumbers);
+
+// "divide" function implementation
+function multiply(str1, str2) {}
+
+const quotientOfNumbers = divide(num1, num2);
+console.log(quotientOfNumbers);
