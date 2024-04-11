f91ed6722120f8ceb4050c4a5556b8dd18674709
Marta <martasagun95@gmail.com>
2024-04-11 21:32:37 +0200

    feat: implement task 1: Immutability and Pure Functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('SLVD HW');
+const products = [
+	{ id: 1, price: 100 },
+	{ id: 1, price: 200 },
+	{ id: 1, price: 300 },
+];
+
+// Task 1: Immutability and Pure Functions
+function calculateDiscountedPrice(arrayOfProducts, discountPercentage) {
+	const newArrayOfProducts = arrayOfProducts.map((product) => {
+		const discountPrice =
+			product.price - (product.price * discountPercentage) / 100;
+		return { ...product, price: discountPrice };
+	});
+
+	return newArrayOfProducts;
+}
+
+function calculateTotalPrice(arrayOfProducts) {
+	const totalPrice = arrayOfProducts.reduce((sum, { price }) => sum + price, 0);
+	return totalPrice;
+}
+
+console.log(calculateDiscountedPrice(products, 10));
+console.log(calculateTotalPrice(products));
