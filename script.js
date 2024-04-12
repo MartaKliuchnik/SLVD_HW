5777e578d57bbccf44287cf8e20a87427a72c974
Marta <martasagun95@gmail.com>
2024-04-12 22:15:52 +0200

    feat: implement task 2: Function Composition and Point-Free Style

--- script.js
+++ script.js
@@ -20,5 +20,95 @@ function calculateTotalPrice(arrayOfProducts) {
 	return totalPrice;
 }
 
-console.log(calculateDiscountedPrice(products, 10));
-console.log(calculateTotalPrice(products));
+// console.log(calculateDiscountedPrice(products, 10));
+// console.log(calculateTotalPrice(products));
+
+// Task 2: Function Composition and Point-Free Style
+// 2.1
+const person = { firstName: 'Billy', lastName: 'Smith' };
+
+const getFirstName = (personObject) => `${personObject.firstName}`;
+const getLastName = (personObject) => `${personObject.lastName}`;
+
+const getConcatenationName = (firstName, lastName) =>
+	`${firstName} ${lastName}`;
+
+const compose1 = function (fn1, fn2, fn3) {
+	return function (personObject) {
+		return fn3(fn1(personObject), fn2(personObject));
+	};
+};
+
+const getFullName = compose1(
+	getFirstName,
+	getLastName,
+	getConcatenationName
+)(person);
+// console.log(getFullName);
+
+//  2.2
+const randomText = 'Hello! hello *JavaScript*, an example hello World';
+
+const getStringFromArray = (array) => {
+	return array
+		.toLowerCase()
+		.replace(/[^\w\s]/g, '')
+		.split(' ');
+};
+
+const getUniqueWords = (array) =>
+	array.reduce(
+		(uniqueWords, word) =>
+			uniqueWords.includes(word) ? uniqueWords : [...uniqueWords, word],
+		[]
+	);
+
+const getAlphabeticalOrder = (array) => array.sort();
+
+const compose2 = function (fn1, fn2, fn3) {
+	return function (initialArray) {
+		return fn1(fn2(fn3(initialArray)));
+	};
+};
+
+const filterUniqueWords = compose2(
+	getAlphabeticalOrder,
+	getUniqueWords,
+	getStringFromArray
+)(randomText);
+// console.log(filterUniqueWords);
+
+// 2.3
+const arrayOfStudent = [
+	{ name: 'Billy', grades: [4, 4, 4, 5, 5] },
+	{ name: 'Tommy', grades: [5, 5, 5, 5] },
+];
+
+const getStudent = (students, fn) =>
+	students.map((student) => fn(student.grades));
+
+const getGradeForStudent = (studentGrade) => {
+	return (
+		studentGrade.reduce((sum, grade) => sum + grade, 0) / studentGrade.length
+	);
+};
+
+const getTotalGrade = (arrayOfGrades) => {
+	return (
+		arrayOfGrades.reduce((sum, grades) => sum + grades, 0) /
+		arrayOfGrades.length
+	);
+};
+
+const compose3 = function (fn1, fn2, fn3) {
+	return function (arrayOfStudent) {
+		return fn3(fn1(arrayOfStudent, fn2));
+	};
+};
+
+const getAverageGrade = compose3(
+	getStudent,
+	getGradeForStudent,
+	getTotalGrade
+)(arrayOfStudent);
+console.log(getAverageGrade);
