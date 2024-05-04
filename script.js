6b51e6d8f566ac37202216abb6f459227f86d291
Marta <martasagun95@gmail.com>
2024-05-04 17:13:18 +0200

    feat: implement task 4: Implement promisify Function

--- script.js
+++ script.js
@@ -19,12 +19,49 @@ function chainPromises(array) {
 	);
 }
 
-const functionsArray = [asyncFunction1, asyncFunction2, asyncFunction3];
-chainPromises(functionsArray)
+// const functionsArray = [asyncFunction1, asyncFunction2, asyncFunction3];
+// chainPromises(functionsArray)
+// 	.then((result) => {
+// 		console.log('Chained promise result:', result);
+// 		// Expected: "Result from asyncFunction1 - Result from asyncFunction2 - Result from asyncFunction3"
+// 	})
+// 	.catch((error) => {
+// 		console.error('Chained promise error:', error);
+// 	});
+
+// Task 4: Implement promisify Function
+function callbackStyleFunction(value, callback) {
+	setTimeout(() => {
+		if (value > 0) {
+			callback(null, value * 2);
+		} else {
+			callback('Invalid value', null);
+		}
+	}, 1000);
+}
+
+function promisify(fn) {
+	return (...args) => {
+		return new Promise((resolve, reject) => {
+			function customCallback(err, ...results) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(results ? results[0] : results);
+				}
+			}
+
+			args.push(customCallback);
+			fn.call(this, ...args);
+		});
+	};
+}
+
+const promisedFunction = promisify(callbackStyleFunction);
+promisedFunction(3)
 	.then((result) => {
-		console.log('Chained promise result:', result);
-		// Expected: "Result from asyncFunction1 - Result from asyncFunction2 - Result from asyncFunction3"
+		console.log('Promised function result:', result); // Expected: 6
 	})
 	.catch((error) => {
-		console.error('Chained promise error:', error);
+		console.error('Promised function error:', error);
 	});
