6bb21a94dd240cf1c07f4f42e92d4c96269edcc3
Marta <martasagun95@gmail.com>
2024-05-08 14:35:31 +0200

    feat: implement Demonstration and Documentation parts

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Part 1: Class Design
 
-//  Represents a book.
+/**
+ * Represents a book (Base class) in the online bookstore.
+ * Encapsulates book-related data like title, author, isbn, price, and availability.
+ * Provides methods to retrieve and manipulate book information.
+ * Manages operations related to displaying details about the book.
+ */
 class Book {
 	/**
 	 * Creates a new instance of Book.
@@ -21,14 +26,6 @@ class Book {
 
 	getTitle() {
 		return this.title;
-	}
-
-	getAuthor() {
-		return this.author;
-	}
-
-	getIsbn() {
-		return this.isbn;
 	}
 
 	getPrice() {
@@ -61,7 +58,36 @@ class Book {
 	}
 }
 
-//  Represents an user
+/**
+ * Represents a subclass of Book, that specifically models a fiction book.
+ * Inherits properties and methods from the Book class.
+ * Encapsulates details of a fiction book, including its type ('Fiction').
+ */
+class FictionBook extends Book {
+	constructor(title, author, isbn, price, availability) {
+		super(title, author, isbn, price, availability);
+		this.type = 'Fiction'; // represents a specific type of book (fiction)
+	}
+}
+
+/**
+ * Represents a subclass of Book, that specifically models a non-fiction book.
+ * Inherits properties and methods from the Book class.
+ * Encapsulates details of a non-fiction book, including its type ('Non-fiction').
+ */
+class NonFictionBook extends Book {
+	constructor(title, author, isbn, price, availability) {
+		super(title, author, isbn, price, availability);
+		this.type = 'Non-fiction'; // represents a specific type of book (non-fiction)
+	}
+}
+
+/**
+ * Represents a user in the online bookstore.
+ * Encapsulates user-related data including name, email, and unique user ID.
+ * Provides methods for interacting with the bookstore, such as browsing books and displaying user information.
+ * Manages interactions with the bookstore, including adding books to the cart and placing orders.
+ */
 class User {
 	/**
 	 * Creates a new instance of User.
@@ -74,6 +100,45 @@ class User {
 		this.name = name;
 		this.email = email;
 		this.userId = userId;
+		this.cart = new Cart(this); // Add own personal's cart for each User in bookstore
+	}
+
+	/**
+	 * Simulates a user browsing a specific book.
+	 * @param {Book} book - The book being browsed by the user.
+	 * @returns {undefined} - This method does not return any value.
+	 */
+	browseBook(book) {
+		console.log(
+			`${this.name}, you are interested in "${book.title}" by ${
+				book.author
+			}, which cost $${book.price}
+			\n${
+				book.availability
+					? 'This book available at this moment'
+					: 'Anvortunately, this book unavaibale at this moment'
+			}`
+		);
+	}
+
+	/**
+	 * Add a book to the user's cart.
+	 * @param {Book} book - The book to add to the cart.
+	 * @returns {Cart} - The updated cart object after adding the book.
+	 */
+	addToCart(book) {
+		this.cart.addBook(book);
+		console.log(`"${book.getTitle()}" book has added to ${this.name}'s cart.`);
+		return this.cart;
+	}
+
+	/**
+	 * Moves the contents of the user's cart to an order.
+	 * @returns {Order} - The order created from the user's cart contents.
+	 */
+	addToOrder() {
+		console.log(this.cart);
+		return this.cart.createNewOrder(this.cart);
 	}
 
 	/**
@@ -88,7 +153,12 @@ class User {
 	}
 }
 
-//  Represents the shopping cart.
+/**
+ * Represents the user's shopping cart in the online bookstore.
+ * Encapsulates the list of books added to the cart.
+ * Provide methods for adding/removing books from the cart and calculating the total price.
+ * Allows the creation of an order associated with a user's cart.
+ */
 class Cart {
 	/**
 	 * Creates a new instance of Cart.
@@ -101,13 +171,16 @@ class Cart {
 	}
 
 	/**
-	 * Adds a book to the cart.
+	 * Add a book to the cart.
 	 * @param {Book} book - The book to add to the cart.
 	 * @returns {Array<Book>} - The updated list of books in the cart.
 	 */
-	addBooks(newBook) {
+	addBook(newBook) {
 		if (newBook.isAvailabile()) {
+			console.log(`"${newBook.title}" has added to ${this.user.name}'s cart.`);
 			this.arrayOfBooks.push(newBook);
+		} else {
+			console.log(`"${newBook.title}" is currently out of stock.`);
 		}
 		return this.arrayOfBooks;
 	}
@@ -117,7 +190,7 @@ class Cart {
 	 * @param {string} book - The book will be removed from the cart.
 	 * @returns {Array<Book>} - The updated list of books in the cart.
 	 */
-	removeBooks(removedBook) {
+	removeBook(removedBook) {
 		this.arrayOfBooks = this.arrayOfBooks.filter(
 			({ isbn }) => isbn !== removedBook.isbn
 		);
@@ -134,9 +207,22 @@ class Cart {
 			0
 		);
 	}
+
+	/**
+	 * Creates and returns the current user's order.
+	 * @returns {Order} - The order created for the current user.
+	 */
+	createNewOrder() {
+		console.log(`${this.user.name}'s order has created`);
+		return new Order(this.user, this.arrayOfBooks);
+	}
 }
 
-// Represent the user's order.
+/**
+ * Represent the user's order in the online bookstore.
+ * Encapsulates order-related data like user, list of ordered books, and total order's price.
+ * Manages operations related to displaying details about the user's order.
+ */
 class Order {
 	/**
 	 * Creates a new instance of an Order.
@@ -156,6 +242,7 @@ class Order {
 	 * @returns {Array<Book>} - The list of available books.
 	 */
 	filterAvailableBooks(books) {
+		console.log(this.orderedBooks);
 		return books.filter((book) => book.availability);
 	}
 
@@ -197,8 +284,8 @@ const book2 = new Book(
 	false
 );
 const book3 = new Book('Burmese Days', 'George Orwell', '9780451524567', 20.2);
-console.log(book1.displayDetails());
-console.log(book2.getPrice().toFixed(2));
+// console.log(book1.displayDetails());
+// console.log(book2.getPrice().toFixed(2));
 
 // Create a few User objects
 const userJohn = new User('John', 'john.doe@example.com', 1);
@@ -209,15 +296,15 @@ console.log(userPeter.displayDetails());
 const shoppingCartJohn = new Cart(userJohn); // Creating instances of the Cart class for John
 const shoppingCartPeter = new Cart(userPeter); // Creating instances of the Cart class for Peter
 
-shoppingCartJohn.addBooks(book1); // Add books for John's cart
-shoppingCartJohn.addBooks(book2); // This book isn't available (can't add to the cart)
-shoppingCartJohn.addBooks(book3);
+shoppingCartJohn.addBook(book1); // Add books for John's cart
+shoppingCartJohn.addBook(book2); // This book isn't available (can't add to the cart)
+shoppingCartJohn.addBook(book3);
 
-shoppingCartJohn.removeBooks(book3); // Remove book from John's cart
+shoppingCartJohn.removeBook(book3); // Remove book from John's cart
 console.log(shoppingCartJohn);
 
-shoppingCartPeter.addBooks(book3); // Add book for Peter's cart
-shoppingCartPeter.addBooks(book1);
+shoppingCartPeter.addBook(book3); // Add book for Peter's cart
+shoppingCartPeter.addBook(book1);
 console.log(shoppingCartPeter);
 
 console.log(shoppingCartPeter.calculateTotalPrice()); // Get total price for book at the Peter's cart
@@ -228,3 +315,56 @@ const orderedBooksForJohn = [book1, book2];
 const orderJohn = new Order(userJohn, orderedBooksForJohn); // Create a new order for user John
 console.log(orderJohn.displayOrderDetails());
 console.log(orderJohn.calculateTotalPrice());
+
+// Part 3: Demonstration
+
+/** Simulating interactions between users, carts, and orders while directly using user methods.
+ * - browseBook(book): Browse a book
+ * - addToCart(book): Adds a book to the user's cart using the User instance.
+ * - addToOrder(): Creates a new order for the user using the User instance.
+ */
+
+userJohn.browseBook(book1);
+
+// Creates a new instance of Cart for John and adds a book to his cart (user interacts with a cart).
+const shoppingCartForJohn = userJohn.addToCart(book1);
+console.log(shoppingCartForJohn);
+
+// Creates a new instance of Order for John based on his cart contents.
+const newOrderJohn = userJohn.addToOrder();
+console.log(newOrderJohn);
+
+// Interaction instance of Cart for Mark with orders. User Mark adds books to the cart and creates a new order.
+const shoppingCartMark = userMark.addToCart(book2); // Add an unavailable book
+const newOrderMark = shoppingCartMark.createNewOrder();
+newOrderMark.displayOrderDetails();
+
+// Polymorphism
+
+// Create instances of FictionBook and NonFictionBook
+const fictionBook1 = new FictionBook(
+	'The Alchemist',
+	'Paulo Coelho.',
+	9780743273000,
+	24.5
+);
+const fictionBook2 = new FictionBook(
+	'The Great Gatsby',
+	'F. Scott Fitzgerald',
+	9780743273001,
+	35.7
+);
+
+// Create instance of NonFictionBook
+const NonFictionBook1 = new NonFictionBook(
+	'Code Dependent',
+	'Madhumita Murgia',
+	9780743272401,
+	45.9,
+	false
+);
+
+// FictionBook and NonFictionBook classes are based on (inherit from) Book, have access to book methods
+console.log(fictionBook1.formattedPrice); // Access inherited method 'formattedPrice' from Book class
+console.log(fictionBook2.displayDetails()); // Access inherited method 'displayDetails' from Book class
+console.log(NonFictionBook1.isAvailabile()); // Check availability using inherited method 'isAvailable' from Book class
